0f912dd1adf39072e98e7dbcf4edae9e117ce040
zwy <zwy@citybaytech.com>
2026-06-11 16:55:26 +0800

    Remove node_modules from git tracking

--- node_modules/@dimforge/rapier3d-compat/control/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from "./character_controller";
-export * from "./pid_controller";
-export * from "./ray_cast_vehicle_controller";
--- node_modules/@dimforge/rapier3d-compat/exports.d.ts
+++
@@ -1,8 +0,0 @@
-export declare function version(): string;
-export declare function reserveMemory(extraBytesCount: number): void;
-export * from "./math";
-export * from "./dynamics";
-export * from "./geometry";
-export * from "./pipeline";
-export * from "./init";
-export * from "./control";
